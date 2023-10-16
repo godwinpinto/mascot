4c3ce42fdfc105130433a9fa57e819378a6b3bdc
Godwin Pinto <godwin.pinto86@gmail.com>
2023-10-16 11:40:54 +0530

    changes

--- vertex-api/index.js
+++ vertex-api/index.js
@@ -1,4 +1,5 @@
 const express = require('express'), 
+const axios = require('axios')
 app = express(); 
   
 app.use(express.urlencoded({ extended: true })) 
@@ -6,6 +7,20 @@ app.use(express.json())
   
 app.get('/',  
    (req, res) => res.send('Dockerizing Node Application')) 
+
+   app.get('/get-google-token', async (req, res) => {
+    try {
+      const response = await axios.get('http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token', {
+        headers: {
+          'Metadata-Flavor': 'Google'
+        }
+      });
+  
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
   
 app.listen(80,  
    () => console.log(`⚡️[bootup]: Server is running at port: 5000`));
